42bc6c8d2ec6dc717681b5f6e904e691f4ae2e3f
LuisDiazOSXminiCDIS <99diazluisfernand@gmail.com>
2024-02-09 16:11:10 -0600

    add func hours and min sleep

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,6 +43,22 @@ export class AppComponent implements OnInit {
       const dataSleep = await this.healthKitService.getActivitySleep();
       this.hora=dataSleep.resultData[0].duration.toString();
 
+      //Se hace el conteo total de las horas en AsLeep (dormido incluyendo REM(Movimientos oculares rapidos) y Deep)
+      const registrosDormidos = dataSleep.resultData.filter(registro => registro.sleepState === "Asleep");
+
+      //Duración total de sueño en minutos
+      const duracionTotalEnMinutos = Math.ceil(registrosDormidos.reduce((total, registro) => {
+        const startDate = new Date(registro.startDate);
+        const endDate = new Date(registro.endDate);
+        const duracionEnMinutos = (endDate.getTime() - startDate.getTime()) / (1000 * 60); // Convertir de milisegundos a minutos
+        return total + duracionEnMinutos;
+      }, 0));
+
+      const horasTotales = Math.floor(duracionTotalEnMinutos / 60);
+      const minutosTotales = duracionTotalEnMinutos % 60;
+      console.log("Recuento"+horasTotales+" horas y "+minutosTotales+" minutos");
+      
+
       const dataWeight = await this.healthKitService.getWeight();
       this.peso = dataWeight.resultData[0].value.toString();
 
